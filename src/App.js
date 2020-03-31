24942100ca192cacae086ca73cc52defa971127e
ceecyl.lang@gmail.com <ceecyl.lang@gmail.com>
2020-03-31 15:46:53 +0200

    quetes 2.2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
+    <>
     <div className="App">
       <Contact
         name="Tiffany Morales"
         avatar="https://randomuser.me/api/portraits/women/76.jpg"
+        Status="online"
       />
     </div>
+    <div className="App">
+      <Contact
+        name="Jacqueline Riley"
+        avatar="https://randomuser.me/api/portraits/women/66.jpg"
+        Status="offline"
+      />
+    </div>
+    <div className="App">
+      <Contact
+        name="Louella Woods"
+        avatar="https://randomuser.me/api/portraits/women/34.jpg"
+        Status="online"
+      />
+    </div>
+    </>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./style.css";
 import PropTypes from "prop-types";
 
-function Contact({ name, avatar, online }) {
+function Contact({ name, avatar, Status }) {
   return (
     <div className="Contact">
       <img className="avatar" src={avatar} alt={name} />
       <div>
         <p className="name">{name}</p>
         <div className="status">
-          <div className={online ? "status-online" : "status-offline"} />
-          <p className="status-text">{online ? "online" : "offline"}</p>
+          <div className={Status === "online" ? "status-online" : "status-offline"} />
+          <p className="status-text">{Status}</p>
         </div>
       </div>
     </div>
